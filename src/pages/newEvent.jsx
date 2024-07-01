cf1fb89ffca18c317c87c2cec020da232a31c0b3
Hulcsipulcsi <162151745+lilihullam@users.noreply.github.com>
2024-07-01 14:34:06 +0200

    pls mukodj mar

--- src/pages/newEvent.jsx
+++ src/pages/newEvent.jsx
@@ -70,7 +70,7 @@ const NewEvent = ({ user }) => {
             //navigate("/events");
         } catch (e) {
             setError("Valami hiba történt")
-            console.log(e.message);
+            console.log(e.message); 
         }
 
     };
