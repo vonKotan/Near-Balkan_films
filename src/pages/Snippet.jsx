1fde823e2267622d60f815b3f98812fb82d2aacc
Hulcsipulcsi <162151745+lilihullam@users.noreply.github.com>
2024-10-01 22:27:41 +0200

    Update Snippet.jsx

--- src/pages/Snippet.jsx
+++ src/pages/Snippet.jsx
@@ -20,8 +20,10 @@ import { Item } from "@radix-ui/react-navigation-menu";
 const User = ({ user }) => {
   return (
     <>
-      <div className="gap-8 grid md:grid-cols-3 grid-flow-row md:grid-flow-col min-w-full">
-        
+      <div className= "flex flex-row flex-wrap justify-center items-center" >
+        <div className="container lg:flex items-stretch bg-nbgreylight mx-auto lg:mx-0 px-5 sm:py-2 pt-2 pb-3 rounded-lg w-full min-w-fit lg:max-w-5xl">
+          <div className="base lg:flex-auto px-2 sm:px-6 py-5 sm:py-7 lg:p-10 sm:pb-5 lg:pb-10"></div>
+        </div>
       </div>
     </>
   );
