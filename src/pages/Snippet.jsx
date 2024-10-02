f2a16b06bf2b01cc8b570e2aa7c91496e1c3b63a
Hulcsipulcsi <162151745+lilihullam@users.noreply.github.com>
2024-10-02 22:10:00 +0200

    Update Snippet.jsx

--- src/pages/Snippet.jsx
+++ src/pages/Snippet.jsx
@@ -26,35 +26,60 @@ const User = ({ user }) => {
             <div className="titleCategories">
               <div className="title justify-center flex-start gap-8 inline-flex items-center w-full">
                 <div className="flex-auto bg-nbgreenmain h-1"></div>
-                <h2 className="headingTitle font-black font-h2-title text-3xl text-nbgreendark leading-tight tracking-tight">Improvisational Scene Structure</h2>
+                <h2 className="headingTitle font-black font-h2-title text-3xl text-nbgreendark leading-tight tracking-tight">
+                  Improvisational Scene Structure
+                </h2>
                 <div className="flex-auto bg-nbgreenmain h-1"></div>
               </div>
               <div className="categories justify-center flex-start gap-2 inline-flex items-center w-full">
-              <h3 className="font-bold font-h3-subtitle text-base text-nbgreenmain tracking-tighter">#screenwriting</h3>
-              <div className="flex-auto bg-nbgreenmain h-0.5 max-w-2"></div>
-              <h3 className="font-bold font-h3-subtitle text-base text-nbgreenmain tracking-tighter">#story structure</h3>
+                <h3 className="font-bold font-h3-subtitle text-base text-nbgreenmain tracking-tighter">
+                  #screenwriting
+                </h3>
+                <div className="flex-auto bg-nbgreenmain h-0.5 max-w-2"></div>
+                <h3 className="font-bold font-h3-subtitle text-base text-nbgreenmain tracking-tighter">
+                  #story structure
+                </h3>
               </div>
             </div>
-            <div className="creatorInfo flex justify-between px-2 sm:px-6 py-5 sm:py-7 lg:p-10 sm:pb-5 lg:pb-10">
-              <div className="snippetAuthors">
-              <div className="snippetAuthor relative">
-                <img className="avatar w-20 h-20 rounded-full" src="https://scontent.fbud8-1.fna.fbcdn.net/v/t39.30808-6/461030322_2306234669728302_5885443326522953977_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=YuqrXw-nSq4Q7kNvgGoIKkX&_nc_ht=scontent.fbud8-1.fna&_nc_gid=AsP1gRkaXVcYF_ZBSEhMiX1&oh=00_AYDrNUMrQyQjSoFT0BmoCIOSRyLHcSqg-U0ljDWc4VgqbA&oe=67034E1B" alt=""></img>
-                <span className="flag bottom-0 left-17 absolute w-6 h-6 border-2 border-nbwhitemain rounded-full display:inline-block overflow-hidden">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGWqznwaynEDGBlgCMkbib4Rm1Wsgk0j_BtA&s" alt="" className="w-full h-5 object-fill"/>
-                </span>
+            <div className="creatorInfo flex flex-col md:flex-row lg:flex-row justify-between items-center px-2 sm:px-6 py-5 sm:py-7 lg:p-10 sm:pb-5 lg:pb-10">
+              <div className="snippetAuthors flex items-center my-2">
+                <div className="snippetAuthor relative m-2.5">
+                  <img
+                    className="avatar w-24 h-24 rounded-full"
+                    src="https://scontent.fbud8-1.fna.fbcdn.net/v/t39.30808-6/461030322_2306234669728302_5885443326522953977_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=YuqrXw-nSq4Q7kNvgGoIKkX&_nc_ht=scontent.fbud8-1.fna&_nc_gid=AsP1gRkaXVcYF_ZBSEhMiX1&oh=00_AYDrNUMrQyQjSoFT0BmoCIOSRyLHcSqg-U0ljDWc4VgqbA&oe=67034E1B"
+                    alt=""
+                  ></img>
+                  <span className="flag bottom-0 left-17 absolute w-6 h-6 border-2 border-nbwhitemain rounded-full display:inline-block overflow-hidden">
+                    <img
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGWqznwaynEDGBlgCMkbib4Rm1Wsgk0j_BtA&s"
+                      alt=""
+                      className="w-full h-5 object-cover"
+                    />
+                  </span>
                 </div>
-                <h4 className="font-bold font-button  text-base text-nbgreendark tracking-tighter">I. M. Bizzer</h4>
+                <div className="flex flex-col">
+                  <h4 className="font-bold font-button text-2xl text-nbgreendark">
+                    I. M. Bizzer
+                  </h4>
+                  <p className="text-nbgreendark leading-4">Near-Balkan</p>
+                </div>
               </div>
-              <div className="ogAuthor">
-                
+              <div className="originalAuthors flex flex-col justify-start mt-4">
+                <div className="authorsTitle">
+                  <p className="text-nbgreendark font-bold font-button text-lg leading-4 bg-nbgreenlight rounded-md min-w-fitmt-3 px-3 py-2">
+                    Original Authors
+                  </p>
+                </div>
+                <div className="authorNames p-1">
+                  <p className="text-nbgreendark leading-4">Nikolay Nikolaev</p>
+                  <p className="text-nbgreendark leading-4">Svetlozar Nachev</p>
+                </div>
               </div>
             </div>
             <div className="problemSolution">
               <div className="problem"></div>
               <div className="explanationArrow">
-                <div className="explanation">
-                
-                </div>
+                <div className="explanation"></div>
                 <div className="arrow"></div>
               </div>
               <div className="solution"></div>
