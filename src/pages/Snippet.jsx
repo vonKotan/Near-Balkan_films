5599151069abd6e81e6a3080166702353cad17bb
Hulcsipulcsi <162151745+lilihullam@users.noreply.github.com>
2024-10-02 20:21:57 +0200

    Update Snippet.jsx

--- src/pages/Snippet.jsx
+++ src/pages/Snippet.jsx
@@ -24,25 +24,30 @@ const User = ({ user }) => {
         <div className="container lg:flex items-stretch bg-nbgreylight mx-auto lg:mx-0 px-5 sm:py-2 pt-2 pb-3 rounded-lg w-full min-w-fit lg:max-w-5xl">
           <div className="base lg:flex-auto px-2 sm:px-6 py-5 sm:py-7 lg:p-10 sm:pb-5 lg:pb-10">
             <div className="titleCategories">
-              <div className="title flex items-center gap-x-4">
-                <div className="flex-auto bg-nbgreenmain h-2"></div>
-                <h3 className="headingTitle font-black font-h2-title text-2xl text-nbgreendark tracking-tight">Improvisational Scene Structure</h3>
-                <div className="flex-auto bg-nbgreenmain h-2"></div>
+              <div className="title justify-center flex-start gap-2 inline-flex items-center w-full">
+                <div className="flex-auto bg-nbgreenmain h-1"></div>
+                <h2 className="headingTitle font-black font-h2-title text-2xl text-nbgreendark tracking-tight">Improvisational Scene Structure</h2>
+                <div className="flex-auto bg-nbgreenmain h-1"></div>
               </div>
-              <div className="categories justify-center flex-start gap-2">
-              <h4 className="font-bold font-h3-subtitle text-base text-nbgreenmain tracking-tighter">#screenwriting</h4>
-              <div className="flex-auto bg-nbgreenmain h-0.5"></div>
-              <h4 className="font-bold font-h3-subtitle text-base text-nbgreenmain tracking-tighter">#story structure</h4>
+              <div className="categories justify-center flex-start gap-2 inline-flex items-center w-full">
+              <h3 className="font-bold font-h3-subtitle text-base text-nbgreenmain tracking-tighter">#screenwriting</h3>
+              <div className="flex-auto bg-nbgreenmain h-0.5 max-w-2"></div>
+              <h3 className="font-bold font-h3-subtitle text-base text-nbgreenmain tracking-tighter">#story structure</h3>
               </div>
             </div>
-            <div className="creatorInfo">
+            <div className="creatorInfo flex justify-between">
+              <div className="snippetAuthors">
               <div className="snippetAuthor relative">
-                <img className="avatar w-20 h-20 rounded-full" src="" alt=""></img>
+                <img className="avatar w-20 h-20 rounded-full" src="https://scontent.fbud8-1.fna.fbcdn.net/v/t39.30808-6/461030322_2306234669728302_5885443326522953977_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=YuqrXw-nSq4Q7kNvgGoIKkX&_nc_ht=scontent.fbud8-1.fna&_nc_gid=AsP1gRkaXVcYF_ZBSEhMiX1&oh=00_AYDrNUMrQyQjSoFT0BmoCIOSRyLHcSqg-U0ljDWc4VgqbA&oe=67034E1B" alt=""></img>
                 <span className="flag bottom-0 left-17 absolute w-6 h-6 border-2 border-nbwhitemain rounded-full display:inline-block overflow-hidden">
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Flag_of_Hungary.svg/2560px-Flag_of_Hungary.svg.png" alt="" className="inline-block w-6 h-6"/>
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGWqznwaynEDGBlgCMkbib4Rm1Wsgk0j_BtA&s" alt="" className="w-full h-5 object-fill"/>
                 </span>
+                </div>
+                <h4 className="font-bold font-button font-bold text-base text-nbgreendark tracking-tighter">I. M. Bizzer</h4>
               </div>
-              <div className="ogAuthor"> </div>
+              <div className="ogAuthor">
+                
+              </div>
             </div>
             <div className="problemSolution">
               <div className="problem"></div>
