575b2c4f3c619dac5f068ac261f821c0bd6a4a9b
N2zoltan <nagy.zoltan0205@gmail.com>
2024-10-03 17:31:39 +0200

    solution
    solution, problem, arrow

--- src/pages/Snippet.jsx
+++ src/pages/Snippet.jsx
@@ -21,35 +21,35 @@ const User = ({ user }) => {
   return (
     <>
       <div className="flex flex-row flex-wrap justify-center items-center">
-        <div className="container lg:flex items-stretch bg-nbgreylight mx-auto lg:mx-0 px-5 sm:py-2 pt-2 pb-3 rounded-lg w-full min-w-fit lg:max-w-5xl">
-          <div className="base lg:flex-auto px-2 sm:px-6 py-5 sm:py-7 lg:p-10 sm:pb-5 lg:pb-10">
+        <div className="lg:flex items-stretch bg-nbgreylight mx-auto lg:mx-0 px-5 sm:py-2 pt-2 pb-3 rounded-lg w-full min-w-fit lg:max-w-5xl container">
+          <div className="lg:flex-auto px-2 sm:px-6 py-5 sm:py-7 lg:p-10 sm:pb-5 lg:pb-10 base">
             <div className="titleCategories">
-              <div className="title justify-center flex-start gap-8 inline-flex items-center w-full">
+              <div className="inline-flex flex-start justify-center items-center gap-8 w-full title">
                 <div className="flex-auto bg-nbgreenmain h-1"></div>
-                <h2 className="headingTitle font-black font-h2-title text-3xl text-nbgreendark leading-tight tracking-tight">
+                <h2 className="font-black font-h2-title text-3xl text-nbgreendark leading-tight tracking-tight headingTitle">
                   Improvisational Scene Structure
                 </h2>
                 <div className="flex-auto bg-nbgreenmain h-1"></div>
               </div>
-              <div className="categories justify-center flex-start gap-2 inline-flex items-center w-full">
+              <div className="inline-flex flex-start justify-center items-center gap-2 w-full categories">
                 <h3 className="font-bold font-h3-subtitle text-base text-nbgreenmain tracking-tighter">
                   #screenwriting
                 </h3>
-                <div className="flex-auto bg-nbgreenmain h-0.5 max-w-2"></div>
+                <div className="flex-auto bg-nbgreenmain max-w-2 h-0.5"></div>
                 <h3 className="font-bold font-h3-subtitle text-base text-nbgreenmain tracking-tighter">
                   #story structure
                 </h3>
               </div>
             </div>
-            <div className="creatorInfo flex flex-col md:flex-row lg:flex-row justify-between items-center px-2 sm:px-6 py-5 sm:py-7 lg:p-10 sm:pb-5 lg:pb-10">
-              <div className="snippetAuthors flex items-center my-2">
-                <div className="snippetAuthor relative m-2.5">
+            <div className="flex md:flex-row lg:flex-row flex-col justify-between items-center px-2 sm:px-6 py-5 sm:py-7 lg:p-10 sm:pb-5 lg:pb-10 creatorInfo">
+              <div className="flex items-center my-2 snippetAuthors">
+                <div className="relative m-2.5 snippetAuthor">
                   <img
-                    className="avatar w-24 h-24 rounded-full"
+                    className="rounded-full w-24 h-24 avatar"
                     src="https://scontent.fbud8-1.fna.fbcdn.net/v/t39.30808-6/461030322_2306234669728302_5885443326522953977_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=YuqrXw-nSq4Q7kNvgGoIKkX&_nc_ht=scontent.fbud8-1.fna&_nc_gid=AsP1gRkaXVcYF_ZBSEhMiX1&oh=00_AYDrNUMrQyQjSoFT0BmoCIOSRyLHcSqg-U0ljDWc4VgqbA&oe=67034E1B"
                     alt=""
                   ></img>
-                  <span className="flag bottom-0 left-17 absolute w-6 h-6 border-2 border-nbwhitemain rounded-full display:inline-block overflow-hidden">
+                  <span className="display:inline-block bottom-0 left-17 absolute border-2 border-nbwhitemain rounded-full w-6 h-6 overflow-hidden flag">
                     <img
                       src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGWqznwaynEDGBlgCMkbib4Rm1Wsgk0j_BtA&s"
                       alt=""
@@ -64,25 +64,34 @@ const User = ({ user }) => {
                   <p className="text-nbgreendark leading-4">Near-Balkan</p>
                 </div>
               </div>
-              <div className="originalAuthors flex flex-col justify-start mt-4">
+              <div className="flex flex-col justify-start mt-4 originalAuthors">
                 <div className="authorsTitle">
-                  <p className="text-nbgreendark font-bold font-button text-lg leading-4 bg-nbgreenlight rounded-md min-w-fitmt-3 px-3 py-2">
+                  <p className="bg-nbgreenlight px-3 py-2 rounded-md min-w-fit font-bold font-button text-lg text-nbgreendark leading-4">
                     Original Authors
                   </p>
                 </div>
-                <div className="authorNames p-1">
+                <div className="p-1 authorNames">
                   <p className="text-nbgreendark leading-4">Nikolay Nikolaev</p>
                   <p className="text-nbgreendark leading-4">Svetlozar Nachev</p>
                 </div>
               </div>
             </div>
             <div className="problemSolution">
-              <div className="problem"></div>
+              <div className="flex justify-center problem">
+                <div className="flex justify-center bg-nbgreylight px-8 py-2 rounded-full max-w-fit">
+                  <div className="font-a-anchor font-bold text-nbgreydark">DRAMATIC SITUATION ARE USUALLY FLAT</div>
+                  </div>
+                </div>
               <div className="explanationArrow">
-                <div className="explanation"></div>
+                <div className="flex-auto bg-nbgreydark h-1 explanation"></div>
                 <div className="arrow"></div>
+                <div className="flex-auto bg-nbgreydark h-1 explanation"></div>
               </div>
-              <div className="solution"></div>
+              <div className="flex justify-center problem">
+                <div className="flex justify-center bg-nbgreylight px-8 py-2 rounded-full max-w-fit">
+                  <div className="font-a-anchor font-bold text-nbgreydark">WE’VE MADE A DRAMATIC SITUATION IN HALF A MINUTE</div>
+                  </div>
+                </div>
             </div>
           </div>
         </div>
